9e3f383dd5d5022963b7ce8209c6cb324d381e64
Leangseu Kim <lkim@edx.org>
2021-11-29 15:21:43 -0500

    feat: adding esg link as additional column when esg flag enable

--- openassessment/xblock/static/dist/openassessment-studio.js
+++ openassessment/xblock/static/dist/openassessment-studio.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5cdb12f4c895737107d8";
+/******/ 	var hotCurrentHash = "424a6c72c2950511b275";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
